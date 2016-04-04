af5ce79997e81010f2bcbedb4734f1a2da5064f7
Jenniferyaya <jenniferfalanga@Jennifers-MacBook-Pro.local>
2016-04-04 16:32:33 -0400

    Keyframes added.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,19 @@ var $svg = $('svg');
 var $lines = $('.lines');
 var $btn = $('.btn');
 var $engage = $('.engage');
+
+$btn.on('click', function () {
+
+  $lines.addClass('addlines');
+
+  $lines.removeClass('lines');
+
+  $engage.addClass('is-engage');
+
+});
+
+$btn.on('mouseenter', function () {
+
+  $(this).children('path:last-child').css('fill', '#f60');
+
+});
