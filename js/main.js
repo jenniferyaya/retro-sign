653075e15028a044b8cf8a695d62b1f8cc6fe52e
Jenniferyaya <jenniferfalanga@Jennifers-MacBook-Pro.local>
2016-04-05 10:36:22 -0400

    Starting over.

--- js/main.js
+++ js/main.js
@@ -4,8 +4,13 @@ var $btn = $('.btn');
 var $engage = $('.engage');
 
 $svg.on('click', function (e) {
-  $lines.toggleClass('is-on');
-  $engage.toggleClass('engage');
-  $btn.toggleClass('button');
-  $svg.toggleClass('is-pressed');
+  $lines.addClass('addlines');
+  $lines.removeClass('lines');
+  $engage.addClass('is-engage');
+  $svg.toggleClass('is-on');
+  $btn.toggleClass('btn');
+  $svg.addClass('is-on');
+
 });
+
+
